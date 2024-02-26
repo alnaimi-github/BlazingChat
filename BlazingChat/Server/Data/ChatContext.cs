20be547013a6907b3da6a349c3f66dd8d3d6d098
Abdulwasia AL-Nuaimi <136977525+Abdulwasia@users.noreply.github.com>
2024-02-27 00:35:07 +0300

    Add project files.

---
+++ BlazingChat/Server/Data/ChatContext.cs
@@ -0,0 +1,29 @@
+﻿using BlazingChat.Server.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingChat.Server.Data
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext(DbContextOptions<ChatContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=ChatDB.db;");
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Message>(e =>
+            {
+                e.HasOne(m=> m.ToUser).WithMany().OnDelete(DeleteBehavior.NoAction);
+                e.HasOne(m=> m.FromUser).WithMany().OnDelete(DeleteBehavior.NoAction);
+            });
+        }
+    }
+}
